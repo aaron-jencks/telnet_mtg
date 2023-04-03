01fb01c4c7e429002706524d7062e8b2985d52c7
Aaron Jencks <32805004+aaron-jencks@users.noreply.github.com>
2023-04-02 19:36:44 -0500

    added pthread launch error

--- utils/error_handler.c
+++ utils/error_handler.c
@@ -6,6 +6,7 @@ const int ERR_DB = 1;
 const int ERR_SCRYFALL = 2;
 const int ERR_DICT_SIZE = 3;
 const int ERR_MUTEX_INIT = 4;
+const int ERR_PTHREAD = 5;
 
 void handle_memory_error(const char* filename, unsigned int linenum, const void* const ptr) {
     if (!ptr) error_at_line(1, ERR_MEMORY, filename, linenum, "Out of Memory!");
--- utils/error_handler.h
+++ utils/error_handler.h
@@ -6,6 +6,7 @@ extern const int ERR_DB;
 extern const int ERR_SCRYFALL;
 extern const int ERR_DICT_SIZE;
 extern const int ERR_MUTEX_INIT;
+extern const int ERR_PTHREAD;
 
 void handle_memory_error(const char* filename, unsigned int linenum, const void* const ptr);
 
